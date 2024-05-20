ac8f32299cf4dda93493b2fe97e3af16b1899409
Atharva Mane <atharvam99.com>
2024-05-20 14:24:49 +0530

    second commit

--- main.js
+++ main.js
@@ -55,6 +55,7 @@ app.post("/generate", (req, res) => {
 
 app.use(express.static(__dirname + "/public"));
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
     console.log("Running")
 });
